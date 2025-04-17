ca7e7b98ca41294cee3256376baebab25926cb3f
andranios <andreagalap2006@gmail.com>
2025-04-18 00:30:51 +0200

    modification et amélioration

--- pages/artiste.php
+++ pages/artiste.php
@@ -1,22 +1,34 @@
 <?php include '../inc/header.php';?>
-
-    <main>
-       </div>
-       <div class="galerie">
-         <img src="../image/ninho.jpg" alt="artiste" data-description="Ninho ([niɲo]), de son vrai nom William Nzobazola, est un rappeur, chanteur, auteur-compositeur et entrepreneur français, né le 2 avril 1996 à Longjumeau. Il est détenteur du record français du nombre de singles certifiés (300 en 2023), avec 187 singles d'or, 90 de platine et 50 de diamant en 2023" data-instagram="https://www.instagram.com/ninhosdt/" data-twitter="https://twitter.com/ninhosdt">
-         <img src="../image/pnl.jpg" alt="artiste" data-description="PNL (sigle de Peace N’ Lovés) est un groupe de rap français composé de deux frères : Ademo et N.O.S, de leurs vrais noms Tarik et Nabil Andrieu, nés respectivement le 26 décembre 1986 et le 25 avril 1989, originaires de la cité des Tarterêts à Corbeil-Essonnes." data-instagram="https://www.instagram.com/pnlmusic" data-twitter="https://twitter.com/pnlmusic" >
-         <img src="../image/Serane.jpg" alt="artiste" data-description="Serane, de son vrai nom Nahuel Serrano, est un rappeur français. Son identité musicale se caractérise par son flow nonchalant, parlé et calme, l'utilisation des thèmes tels que l'argent, les relations amoureuses, les femmes, ainsi que des instrumentaux plugg et punk" data-instagram="https://www.instagram.com/sxrxnx___/?hl=fr" data-twitter="https://x.com/sxrxnxbigs" >
-         <img src="../image/keequaid.jpg" alt="artiste" data-description="Qui est Kee dans tes oreilles 93210 Saint Denis jusqu’au Top TrapPlug moi ça sal drilleur C’est tout … JEAN KILLY RECORDS" data-instagram="https://www.instagram.com/keeqaid/?locale=fr_FR&hl=de" data-twitter="https://x.com/keeqaid1"  >
-         <img src="../image/630.jpg" alt="artiste" data-description="63OG est un rappeur français membre du groupe 63WA.Le rappeur développe sa carrière en étant influencé par des artistes provenant d'outre-Atlantique comme Chief Keef, Playboi Carti ou Summrs." data-instagram="https://www.instagram.com/63wog/" data-twitter="https://x.com/63wog?lang=fr" >
-         <img src="../image/shay.jpg" alt="artiste" data-description="Vanessa Lesnicki, dite Shay (prononcé en anglais :/ʃaj/), est une rappeuse et auteure-compositrice-interprète belge, née le 16 août 1992[1] à Bruxelles. Elle fait ses premiers pas de rappeuse en 2011 avec le rappeur Booba sur la chanson Cruella, pour ensuite signer dans son label 92i et sortir son premier album le 2 décembre 2016." data-instagram="https://www.instagram.com/shayizi/?hl=fr" data-twitter="https://x.com/shayizi?lang=fr" >
-        </div>
-    </main>
-
-    
-    
  
-    
+    <main class="galerie">
+       
+       
+         <img src="../image/ninho.jpg" alt="artiste" data-description="Ninho ([niɲo]), de son vrai nom William Nzobazola, est un rappeur, chanteur, auteur-compositeur et entrepreneur français, né le 2 avril 1996 à Longjumeau. Il est détenteur du record français du nombre de singles certifiés (300 en 2023), avec 187 singles d'or, 90 de platine et 50 de diamant en 2023" data-instagram="https://www.instagram.com/ninhosdt/" data-twitter="https://twitter.com/ninhosdt">
+ 
+ 
+         <img src="../image/pnl.jpg" alt="artiste" data-description="PNL (sigle de Peace N’ Lovés) est un groupe de rap français composé de deux frères : Ademo et N.O.S, de leurs vrais noms Tarik et Nabil Andrieu, nés respectivement le 26 décembre 1986 et le 25 avril 1989, originaires de la cité des Tarterêts à Corbeil-Essonnes." data-instagram="https://www.instagram.com/pnlmusic" data-twitter="https://twitter.com/pnlmusic" >
+ 
+ 
+         <img src="../image/Serane.jpg" alt="artiste" data-description="Serane, de son vrai nom Nahuel Serrano, est un rappeur français. Son identité musicale se caractérise par son flow nonchalant, parlé et calme, l'utilisation des thèmes tels que l'argent, les relations amoureuses, les femmes, ainsi que des instrumentaux plugg et punk" data-instagram="https://www.instagram.com/sxrxnx___/?hl=fr" data-twitter="https://x.com/sxrxnxbigs" >
+ 
+ 
+ 
+         <img src="../image/keequaid.jpg" alt="artiste" data-description="Qui est Kee dans tes oreilles 93210 Saint Denis jusqu’au Top TrapPlug moi ça sal drilleur C’est tout … JEAN KILLY RECORDS" data-instagram="https://www.instagram.com/keeqaid/?locale=fr_FR&hl=de" data-twitter="https://x.com/keeqaid1"  >
+ 
+ 
+         <img src="../image/630.jpg" alt="artiste" data-description="63OG est un rappeur français membre du groupe 63WA.Le rappeur développe sa carrière en étant influencé par des artistes provenant d'outre-Atlantique comme Chief Keef, Playboi Carti ou Summrs." data-instagram="https://www.instagram.com/63wog/" data-twitter="https://x.com/63wog?lang=fr" >
+ 
+ 
+         
+         <img src="../image/shay.jpg" alt="artiste" data-description="Vanessa Lesnicki, dite Shay (prononcé en anglais :/ʃaj/), est une rappeuse et auteure-compositrice-interprète belge, née le 16 août 1992[1] à Bruxelles. Elle fait ses premiers pas de rappeuse en 2011 avec le rappeur Booba sur la chanson Cruella, pour ensuite signer dans son label 92i et sortir son premier album le 2 décembre 2016." data-instagram="https://www.instagram.com/shayizi/?hl=fr" data-twitter="https://x.com/shayizi?lang=fr" >
+       
+    </main>
+    <aside></aside>
+ 
+   
+   
+ 
+   
    <script type="text/javascript" src="../js/javascript.js"></script>
    
-
 <?php include '../inc/footer.php';
