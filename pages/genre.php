af2f8c4418c62eb0fbad0a938e5e9af90bab3ef7
Janishmn <157222486+Janishmn@users.noreply.github.com>
2025-04-17 15:53:37 +0200

    ajout de la page genre

---
+++ pages/genre.php
@@ -0,0 +1,43 @@
+<?php include '../inc/header.php'; ?>
+<main>
+    <article>
+        <img src="../image/symbole_music.png" alt="" class="lightbox">
+        <h2>Plug</h2>
+        <audio src="../musique/serane-mybaby.mp3" controls></audio>
+        <p>Un rap mélodique, inspiré de la trap avec des prods aériennes.</p>
+        <img src="../image/bouton-jouer.png" class="ecouter" alt="play">
+        <img src="../image/pause.png" class="pause" alt="pause">
+    </article>
+    <article>
+        <img src="../image/symbole_music.png" alt="" class="lightbox">
+        <h2>Boom-Bap</h2>
+        <audio src="../musique/LRH.mp3" controls></audio>
+        <p>Le rap old-school avec des beats classiques et des textes travaillés.</p>
+        <img src="../image/bouton-jouer.png" class="ecouter" alt="play">
+        <img src="../image/pause.png" class="pause" alt="pause">
+    </article>
+    <article>
+        <img src="../image/symbole_music.png" alt="" class="lightbox">
+        <h2>Drill</h2>
+        <audio src="../musique/1plike140.mp3" controls></audio>
+        <p>Un rap sombre, rythmé par des basses puissantes et des flows saccadés.</p>
+        <img src="../image/bouton-jouer.png" class="ecouter" alt="play">
+        <img src="../image/pause.png" class="pause" alt="pause">
+    </article>
+    <article>
+        <img src="../image/symbole_music.png" alt="" class="lightbox">
+        <h2>Trap</h2>
+        <audio src="../musique/Mafiaspartiate.mp3" controls></audio>
+        <p>Des 808 lourds, des hi-hats rapides et une ambiance club/street.</p>
+        <img src="../image/bouton-jouer.png" class="ecouter" alt="play">
+        <img src="../image/pause.png" class="pause" alt="pause">
+    </article>
+
+
+</main>
+
+<aside></aside>
+<script type="text/javascript" src="../js/musique.js"></script>
+<script type="text/javascript" src="../js/ligthbox.js"></script>
+
+<?php include '../inc/footer.php';
